7302272a678014b249e52048c9d771087aff2d8e
KwonSunwon <kso00011147@gmail.com>
2023-03-14 15:05:13 +0900

    class를 binary mode로 입출력

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,50 +1,57 @@
 //-------------------------------------------------------
 // 2023 1학기 STL 3월 14일 (2주 2)
 //-------------------------------------------------------
-// 많은 수의 자료 다루기 - int
-// int 한 개를 저장하는 데 필요한 메모리 공간은?
+// 많은 수의 자료 다루기 - class
 //-------------------------------------------------------
 // 코딩환경 - VS Release/x64, C++표준 - latest, SDL/아니오
 //-------------------------------------------------------
 
 #include <array>
+#include <fstream>
 #include <iostream>
-#include <sstream>
+#include <random>
 
 #include "save.hpp"
 
-// [문제] 파일 "int 천개"에 있는 int 1000개를 메모리에 저장하라
-// int는 텍스트 형식으로 공백으로 분리되어 저장되어 있다
+std::default_random_engine dre;
+std::uniform_int_distribution<int> uidChar{ 'a', 'z' };
+std::uniform_int_distribution uidNum{ 1, 10'000 };
 
-// contiguous memory(공간적 연속 메모리)
-// continuous(시간적 연속)
+class Dog {
+	// 5byte로 끝낼 수 있지만 속도를 위해 8byte 저장
+	// 빈 공간 패딩
+	char c;
+	int n;
 
-// DATA와 STACK 공간은 떨어져 있다
-std::array<int, 1'000> ga; // DATA
-int gn;
+public:
+	Dog()
+	{
+		c = uidChar(dre); // ['a', 'z']
+		n = uidNum(dre); // [1, 10'000]
+	}
 
-int main()  // CODE/TEXT SEGMENT -> DATA와 위치가 가까움
+	friend std::ostream& operator<<(std::ostream& os, const Dog& dog)
+	{
+		return os << "글자 - " << dog.c << ", 숫자 - " << dog.n;
+	}
+};
+
+// [문제] Dog 1000 객체를 파일 "Dog 천마리"를 binary mode로 열고
+// 메모리 그대로 파일에 기록하라
+
+int main()
 {
-    std::array<int, 1'000> a;
-    std::cout << sizeof a << '\n'; // STACK
-    int n;
+	std::array<Dog, 1'000> dogs;
 
-    std::cout << "STACK에 있는 a의 번지\t- " << std::addressof(a) << '\n';
-    std::cout << "STACK에 있는 n의 번지\t- " << std::addressof(n) << '\n';
-    std::cout << '\n';
+	// std::ofstream out { "Dog 천마리", std::ios::binary };
+	// out.write((char*)&dogs, dogs.size() * sizeof(Dog)); // 8000byte로 저장됨
 
-    int* p = new int {};
-    std::cout << "FreeStore의 있는 번지\t- " << p << '\n'; // 포인터 주소가 가리키는 freestore 데이터의 주소
-    //std::cout << "FreeStore의 있는 번지\t- " << std::addressof(p) << '\n'; // 포인터의 주소
-    std::cout << '\n';
+	std::ifstream in{ "Dog 천마리", std::ios::binary };
+	in.read((char*)&dogs, dogs.size() * sizeof(Dog));
 
-    std::cout << "CODE에 있는 main\t- " << std::addressof(main) << '\n';
-    std::cout << "CODE에 있는 save\t- " << std::addressof(save) << '\n';
-    std::cout << '\n';
+	for (auto& dog : dogs) {
+		std::cout << dog << '\n';
+	}
 
-    // DATA 영역은 두 가지로 나뉜다
-    std::cout << "DATA에 있는 ga의 번지\t- " << std::addressof(ga) << '\n';
-    std::cout << "DATA에 있는 gn의 번지\t- " << std::addressof(gn) << '\n';
-
-    save("main.cpp");
+	 save("main.cpp");
 }
