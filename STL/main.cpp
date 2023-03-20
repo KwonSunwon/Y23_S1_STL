81576eb7ab5414fb95d85fc0a3ff8bcaa0e82999
KwonSunwon <kso00011147@gmail.com>
2023-03-20 11:39:33 +0900

    STACK은 경고를 해주지 않는다

--- STL/main.cpp
+++ STL/main.cpp
@@ -2,6 +2,7 @@
 // 2023 1학기 STL 3월 20일 (3주 1)
 //-------------------------------------------------------
 // STACK의 한계 - 1MB가 default(COMPILER가 코드 생성하지 않는 것 주의)
+// STACK의 한계는 경고해주지 않음
 // 더 큰 데이터 사용 - DATA와 FreeStore(HEAP)
 //-------------------------------------------------------
 // 코딩환경 - VS Release/x64, C++표준 - latest, SDL/아니오
@@ -9,19 +10,15 @@
 
 #include <iostream>
 #include <array>
-#include <format>
-#include <ranges> // C++20
 #include "save.hpp"
-
-std::array<int, 1'990'000'000 / sizeof(int)> a{ 1 }; // DATA segment, 전역공간에서 a; 라고 쓰는 것은 a{}; 과 같다.
-// 1.85GB - 실행 파일의 크기
 
 int main()
 {
-	for (int num : a |
-		std::views::take(100)) {
-		std::cout << std::format("{:10}", num);
-	}
+	std::array<int, 1000> a;
+	a[0] = 123;
+	a[999] = 456;
+	std::cout << a[0] << ", " << a[999] << '\n';
+	main();
 
 	save("main.cpp");
 }
