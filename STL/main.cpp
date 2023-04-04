40758c38b6e5b03062c6401bf16d74396160d72f
KwonSunwon <kso00011147@gmail.com>
2023-04-04 14:43:13 +0900

    5주2: STL에서의 반복문

--- STL/main.cpp
+++ STL/main.cpp
@@ -15,10 +15,14 @@ int main()
 {
 	std::array<int, 10> a{ 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
 
-	// [문제] a의 모든 데이터를 역순으로 화면에 출력하라
-	for (auto p = a.rbegin(); p != a.rend(); ++p) {
-		std::cout << *p << std::endl; // *p == p.operator*()
-	}
+	// [문제] a의 모든 데이터를 화면에 출력하라
+	for (auto p = a.begin(); p != a.end(); ++p) // 어떤 STL 자료구조든 문제 없이 사용 가능 -> 표준
+		std::cout << *p << ' ';
+	std::cout << std::endl;
+
+	for (int i = 0; i < a.size(); ++i) // array 자료구조에 종속저
+		std::cout << a[i] << ' ';
+	std::cout << std::endl;
 
 	save("main.cpp");
 }
