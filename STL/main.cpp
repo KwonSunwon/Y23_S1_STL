44e8379f66055ea71d364631b018db955115eaa4
KwonSunwon <kso00011147@gmail.com>
2023-04-04 14:32:43 +0900

    5주2: array iterator for loop

--- STL/main.cpp
+++ STL/main.cpp
@@ -11,31 +11,14 @@
 #include "save.h"
 #include "String.h"
 
-// Containers are objects that store other objects.
-
 int main()
 {
 	std::array<int, 10> a{ 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
 
 	// [문제] a의 모든 데이터를 화면에 출력하라
-	std::array<int, 10>::iterator p = a.begin();
-	while(p != a.end()) {
+	for (auto p = a.begin(); p != a.end(); ++p) {
 		std::cout << *p << std::endl; // *p == p.operator*()
-		++p;
 	}
 
 	save("main.cpp");
 }
-
-// Seqeunce Container 
-// array
-// vector
-// deque
-// forward-list
-// list
-// - 원하는 위치에 바로 Read & Write 
-
-// Associative Container - O(logN)
-// Unordered Associative Container - O(1)
-
-// Iterator
