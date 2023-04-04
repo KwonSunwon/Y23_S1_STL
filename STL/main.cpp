9ac4bbd5f39ea5a7c533161eb54e9cf83456160b
KwonSunwon <kso00011147@gmail.com>
2023-04-04 14:39:05 +0900

    5주2: 역방향 interator

--- STL/main.cpp
+++ STL/main.cpp
@@ -15,8 +15,8 @@ int main()
 {
 	std::array<int, 10> a{ 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
 
-	// [문제] a의 모든 데이터를 화면에 출력하라
-	for (auto p = a.begin(); p != a.end(); ++p) {
+	// [문제] a의 모든 데이터를 역순으로 화면에 출력하라
+	for (auto p = a.rbegin(); p != a.rend(); ++p) {
 		std::cout << *p << std::endl; // *p == p.operator*()
 	}
 
