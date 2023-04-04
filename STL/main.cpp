67f992a5dc009a9fc9cbacb3351d13702353f7cb
KwonSunwon <kso00011147@gmail.com>
2023-04-04 14:14:40 +0900

    5주2: iterator

--- STL/main.cpp
+++ STL/main.cpp
@@ -15,6 +15,11 @@
 
 int main()
 {
+	std::array<int, 10> a{ 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
+
+	// [문제] a의 모든 데이터를 화면에 출력하라
+	std::array<int, 10>::iterator p = a.begin();
+
 	save("main.cpp");
 }
 
