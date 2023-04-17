a7b4466c140029a95c30f98c62cfc9f87c627ea1
KwonSunwon <kso00011147@gmail.com>
2023-04-17 11:07:07 +0900

    7주1: erase-remove idiom

--- STL/main.cpp
+++ STL/main.cpp
@@ -19,12 +19,18 @@ using namespace std;
 int main()
 {
 	vector<int> v{ 1,2,3,4,5 };
-
+	
 	// [문제] v에서 3을 제거하라.
 	// 알고리즘 함수 remove를 사용하면 된다.
 
-	vector<int>::iterator iter = remove(v.begin(), v.end(), 3);
+	auto iter = remove(v.begin(), v.end(), 3); // 이런 경우에는 auto를 사용한다.
 	v.erase(iter, v.end());
+
+	v.erase(remove(v.begin(), v.end(), 3), v.end()); // 그리고 보통은 이렇게 한다.
+	// -> erase-remove idiom
+	// 근데 매번 이렇게 쓰기 귀찮으니까 C++20 부터는...
+	erase(v, 3); // 새로운 전역함수가 제공된다
+	// 그런데 이거 벡터에서만 돌아감, 그리고 진짜 전역변수가 아닌 syntactic sugar이다.
 
 	for (int i = 0; i < v.size(); ++i)
 		cout << v[i] << ' ';
