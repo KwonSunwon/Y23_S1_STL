321ff15b16e62d13a8be300a5980873e1fc5ee2b
KwonSunwon <kso00011147@gmail.com>
2023-04-18 13:56:25 +0900

    7주2: deque는 contigousus 하지 않다

--- STL/main.cpp
+++ STL/main.cpp
@@ -19,15 +19,12 @@ extern bool 관찰;
 
 int main()
 {
-	// 벡터의 메모리는 contiguous?
-	vector<int> v{ 1,2,3,4,5,6,7,8,9,10 };
+	// 덱의 메모리는 contiguous?
+	deque<int> d{ 1,2,3,4,5,6,7,8,9,10 };
 
-	// v의 메모리가 연속되어 있는지 확인하시오
-	if(v.data() == &v[0])
-		cout << "두 주소는 같다" << endl;
-
-	for (int i = 0; i < v.size(); ++i)
-			cout << addressof(v[i]) << endl;
+	for (int i = 0; i < d.size(); ++i)
+			cout << addressof(d[i]) << endl;
+	// 메모리 주소가 4개씩 연속되어 있다
 	
 	//save("main.cpp");
 }
