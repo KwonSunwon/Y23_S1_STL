ea9c2adf8ec6e830ae6663a32a8e112adfd3d021
KwonSunwon <kso00011147@gmail.com>
2023-04-24 11:06:57 +0900

    8주1: list 정리

--- STL/main.cpp
+++ STL/main.cpp
@@ -2,7 +2,7 @@
 // 2023 1학기 STL 4월 24일 (8주 1)
 // 4월 25일 - (8주 2) 중간시험
 //-------------------------------------------------------
-// Sequence Container - list
+// Sequence Container - list - 멤버로 작성된 함수들을 활용하라.(sort, merge)
 //-------------------------------------------------------
 // 코딩환경 - VS Release/x64, C++표준 - latest, SDL/아니오
 //-------------------------------------------------------
@@ -18,11 +18,12 @@ extern bool 관찰;
 
 int main()
 {
-	list<int> a{ 1, 3, 4, 9, 5, 7 };
-	list<int> b{ 4, 10, 2, 4, 4, 8, 6 };
+	list<int> a{ 1, 3, 9, 5, 7 };
+	list<int> b{ 4, 10, 2, 8, 6 };
 
-	// a와 b를 merge하라
-	a.merge(b);
+	// a와 b를 내림차순으로 정렬
+	a.sort([](int a, int b) { return a > b; });
+	b.sort([](int a, int b) { return a > b; });
 
 	for (auto& n : a)
 		cout << n << ' ';
@@ -30,6 +31,21 @@ int main()
 
 	for (auto& n : b)
 		cout << n << ' ';
+	cout << endl;
+
+	// a와 b를 merge하라
+	cout << "merge" << endl;
+	a.merge(b, [](int a, int b) {
+		return a > b;
+		});
+
+	for (auto& n : a)
+		cout << n << ' ';
+	cout << endl;
+
+	for (auto& n : b)
+		cout << n << ' ';
+	cout << endl;
 
 	//save("main.cpp");
 }
