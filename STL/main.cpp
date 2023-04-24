1a0671318883cf341ba56764ff84080d486ea37e
KwonSunwon <kso00011147@gmail.com>
2023-04-24 11:40:06 +0900

    8주1: span의 등장 계기

--- STL/main.cpp
+++ STL/main.cpp
@@ -2,26 +2,34 @@
 // 2023 1학기 STL 4월 24일 (8주 1)
 // 4월 25일 - (8주 2) 중간시험
 //-------------------------------------------------------
-// Sequence Container - forward_list
-// 직접 작성한 C-style의 단일 연결 리스트보다 조금의 공강이나 시간의
-// 오버헤드가 없도록 설계한 컨테이너이다. 이것에 위반하는 코드는 아예
-// 제공하지 않는다.
+// span, mdspan
 //-------------------------------------------------------
 // 코딩환경 - VS Release/x64, C++표준 - latest, SDL/아니오
 //-------------------------------------------------------
 
 #include <iostream>
-#include <forward_list>
+#include <span>
 #include "save.h"
 #include "String.h"
 
 using namespace std;
-
 extern bool 관찰;
+
+void print(int[]); // void print(int*)
 
 int main()
 {
-	
+	int a[10]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	print(a);  // a의 값을 화면에 출력
 
 	//save("main.cpp");
+}
+
+void print(int a[])
+{
+	//for (int i = 0; i < ???)  // 개수 정보는 절대 넘어오지 않는다.
+	for (int i = 0 ; i < 20; ++i)
+		cout << a[i] << ' ';
+	cout << endl;
 }
