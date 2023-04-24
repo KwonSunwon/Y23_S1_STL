740932d8803bfa0f34c087794ecc3089abf8b809
KwonSunwon <kso00011147@gmail.com>
2023-04-24 11:17:51 +0900

    8주1: forward_list는 간단 정리만

--- STL/main.cpp
+++ STL/main.cpp
@@ -2,13 +2,16 @@
 // 2023 1학기 STL 4월 24일 (8주 1)
 // 4월 25일 - (8주 2) 중간시험
 //-------------------------------------------------------
-// Sequence Container - list - 멤버로 작성된 함수들을 활용하라.(sort, merge)
+// Sequence Container - forward_list
+// 직접 작성한 C-style의 단일 연결 리스트보다 조금의 공강이나 시간의
+// 오버헤드가 없도록 설계한 컨테이너이다. 이것에 위반하는 코드는 아예
+// 제공하지 않는다.
 //-------------------------------------------------------
 // 코딩환경 - VS Release/x64, C++표준 - latest, SDL/아니오
 //-------------------------------------------------------
 
 #include <iostream>
-#include <list>
+#include <forward_list>
 #include "save.h"
 #include "String.h"
 
@@ -18,34 +21,7 @@ extern bool 관찰;
 
 int main()
 {
-	list<int> a{ 1, 3, 9, 5, 7 };
-	list<int> b{ 4, 10, 2, 8, 6 };
-
-	// a와 b를 내림차순으로 정렬
-	a.sort([](int a, int b) { return a > b; });
-	b.sort([](int a, int b) { return a > b; });
-
-	for (auto& n : a)
-		cout << n << ' ';
-	cout << endl;
-
-	for (auto& n : b)
-		cout << n << ' ';
-	cout << endl;
-
-	// a와 b를 merge하라
-	cout << "merge" << endl;
-	a.merge(b, [](int a, int b) {
-		return a > b;
-		});
-
-	for (auto& n : a)
-		cout << n << ' ';
-	cout << endl;
-
-	for (auto& n : b)
-		cout << n << ' ';
-	cout << endl;
+	
 
 	//save("main.cpp");
 }
