ef90f6e47c2609206ccbb95f5731f9264e5e3b09
KwonSunwon <kso00011147@gmail.com>
2023-05-08 11:11:23 +0900

    10주1: String을 역방향으로 출력

--- STL/main.cpp
+++ STL/main.cpp
@@ -7,18 +7,34 @@
 //-------------------------------------------------------
 
 #include <iostream>
+#include <span>
 #include "save.h"
 #include "String.h"
 
 using namespace std;
 
+template<typename T>
+void reverse_print(span<T> s)
+{
+	for(auto p = s.rbegin(); p != s.rend(); ++p)
+		cout << *p << ' ';
+}
+
 int main()
 {
 	String s{ "1234567890" };
 
-	for (auto p = s.begin(); p != s.end(); ++p)
+	// s를 역방향으로 출력하시오
+	for (auto p = s.end(); p != s.begin();)
+		cout << *--p << ' ';
+	cout << endl;
+
+	span<char> sp{ s.begin(), s.end() };
+	for(auto p = sp.rbegin(); p != sp.rend(); ++p)
 		cout << *p << ' ';
 	cout << endl;
+
+	reverse_print(span<char>(s));
 
 	//save("main.cpp");
 }
