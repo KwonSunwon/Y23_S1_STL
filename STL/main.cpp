69da55137a5153a875ef7b982b815e018e7d2611
KwonSunwon <kso00011147@gmail.com>
2023-05-15 11:59:22 +0900

    11주1: less<String> Specialization

--- STL/main.cpp
+++ STL/main.cpp
@@ -4,8 +4,9 @@
 // Associative 컨테이너 = set/map (상등서/동등성)
 // "이상한 나라의 앨리스.txt" - 26634
 // set에 String을 담기 위해...
+// == set의 정렬기준을 설정하는 방법 ==
 // 1. String이 operator<를 제공
-// 2. 
+// 2. less<String>을 specialization
 //-------------------------------------------------------
 // 코딩환경 - VS Release/x64, C++표준 - latest, SDL/아니오
 //-------------------------------------------------------
@@ -18,25 +19,20 @@
 
 using namespace std;
 
+// Specialization, 특수화
+template<>
+struct less<String>{
+	bool operator()(const String& a, const String& b) const {
+		// 길이순 정렬
+		return a.size() < b.size();
+	}
+};
+
 int main()
 {
 	ifstream in{ "이상한 나라의 앨리스.txt" };
-	set<String, less<String>> s{ istream_iterator<String>{in}, {} };
+	set<String> s{istream_iterator<String>{in}, {}};
 	cout << "읽은 개수: " << s.size() << endl;
 
-	// [문제] 사용자가 입력한 단어가 set에 있는지 있다면 몇 번째 단어인지 출력
-
-	while (true) {
-		cout << "찾을 단어를 입력하세요: ";
-		String word;
-		cin >> word;
-
-		auto iter = s.find(word);
-		if(iter != s.end())
-			cout << "찾은 단어 위치: " << distance(s.begin(), iter) + 1 << endl;
-		else
-			cout << "찾지 못했습니다." << endl;
-	}
-
-	save("main.cpp");
+	//save("main.cpp");
 }
