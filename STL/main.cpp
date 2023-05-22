f89476bb2c404b0dc117e7389aadc8859ebe8086
KwonSunwon <kso00011147@gmail.com>
2023-05-22 11:35:37 +0900

    12주1: map 간단 활용

--- STL/main.cpp
+++ STL/main.cpp
@@ -8,8 +8,9 @@
 
 #include <iostream>
 #include <map>
-#include <list>
-#include <utility>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 #include "save.h"
 #include "String.h"
 
@@ -17,38 +18,40 @@ using namespace std;
 
 int main()
 {
-	// 게임회사, 출시작들
-	map<String, list<String>> game{
-		{"펄 어비스", { "검은사막", "붉은사막", "도깨비" }},
-		{ "베데스다", {"폴아웃", "엘더스크롤", "스타필드", "터미네이터"} },
-	};
+	// [문제] "이상한 나라의 앨리스.txt"에 사용된 문자와 사용된 횟수를 출력하라.
+	// a - 100
+	// ...
+	// z - 2
 
-	//game.insert(pair<String, list<String>> {"라이엇", { "리그오브레전드", "발로란트" }});
-	game.insert(make_pair("라이엇", list<String>{ "리그오브레전드", "발로란트" }));
+	ifstream in{ "이상한 나라의 앨리스.txt" };
+	if (!in) exit(0);
 
-	cout << "맵의 원소 수 - " << game.size() << endl;
 
-	// 맵의 키값을 변경하고 싶다면 extract 멤버를 이용한다.(since C++17)
-	auto handle = game.extract("펄 어비스");
-	handle.key() = "펄어비스";
-	game.insert(move(handle));
-
-	// 회사 하나에 게임을 추가한다.
-	//game["라이엇"].push_back("레전드오브룬테라"); // 연관배열(associative array)
-	//list<String>& val = game["라이엇"];
-	auto& val = game["라이엇"]; // 제대로 알고 사용
-	val.insert(val.begin(), "팀파이터");
-
-	//auto it = game.find("라이엇");
-	//if (it != game.end())
-	//	it->second.push_back("레전드오브룬테라");
-
-	for (const auto& [회사, 게임들] : game) {
-		cout << 회사 << " - ";
-		for (const String& 게임 : 게임들)
-			cout << "\"" << 게임 << "\" ";
-		cout << endl;
+	map<char, int> cimap;
+	char c;
+	while (in >> c) {
+		if (isalpha(c))
+			cimap[tolower(c)]++;
 	}
+
+	//for (auto& [문자, 개수] : cimap)
+	//	cout << 문자 << " - " << 개수 << endl;
+
+	// [문제] 많이 사용된 순서로 출력하라
+	/*map<int, char, greater<int>> icmap;
+	for (auto& [문자, 개수] : cimap)
+		icmap[개수] = 문자;
+
+	for (auto& [개수, 문자] : icmap)
+		cout << 문자 << " - " << 개수 << endl;*/
+
+	vector<pair<char, int>> v{ cimap.begin(), cimap.end() };
+	sort(v.begin(), v.end(), [](const auto& a, const auto& b) {
+		return a.second > b.second;
+		});
+
+	for (const auto& [문자, 개수] : v)
+		cout << 문자 << " - " << 개수 << endl;
 
 	save("main.cpp");
 }
