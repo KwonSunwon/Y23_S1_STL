2cbbcd1f06ad5ce7b04388802120af3f5b14d773
KwonSunwon <kso00011147@gmail.com>
2023-05-23 14:00:05 +0900

    12주2: unordered associative containers 공부 목표

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,17 +1,16 @@
 //-------------------------------------------------------
-// 2023 1학기 STL 5월 22일 (12주 1)
+// 2023 1학기 STL 5월 23일 (12주 2)
 //-------------------------------------------------------
-// map
+// unordered associative containers - 순서없는 연관 컨테이너
+// 1. 순서없다는 것을 관찰
+// 2. 메모리 구조를 화면에 출력한다
+// 3. String을 여기에 원소로 집어넣으려면?
 //-------------------------------------------------------
 // 코딩환경 - VS Release/x64, C++표준 - latest, SDL/아니오
 //-------------------------------------------------------
 
 #include <iostream>
-#include <map>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <ranges>
+#include <unordered_set>
 #include "save.h"
 #include "String.h"
 
@@ -19,33 +18,10 @@ using namespace std;
 
 int main()
 {
-	// [문제] "이상한 나라의 앨리스.txt"에 사용된 단어와 상용된 횟수를 출력하라.
-	ifstream in{ "이상한 나라의 앨리스.txt" };
-	if (!in) exit(0);
+	unordered_set<int> us = { 4, 1, 3, 2 };
 
-	map<String, int> Simap;
+	for (int n : us)
+		cout << n << " ";
 
-	String word;
-	while (in >> word)
-		Simap[word]++;
-
-	/*for (auto& [word, count] : Simap)
-		cout << word << " - " << count << endl;*/
-
-	// 많이 사용된 단어기준 내림차순으로 출력하라.
-	/*multimap<int, String, greater<int>> iSmap;
-	for (auto& [word, count] : Simap)
-		iSmap.emplace(count, word);
-
-	for (const auto& [count, word] : iSmap | views::take(100))
-		cout << count << "\t- " << word << endl;*/
-
-	multimap<int, String> iSmap;
-	for (auto& [word, count] : Simap)
-		iSmap.emplace(count, word);
-
-	for (const auto& [count, word] : iSmap | views::reverse | views::take(100))
-		cout << count << "\t- " << word << endl;
-
-	//save("main.cpp");
+	save("main.cpp");
 }
