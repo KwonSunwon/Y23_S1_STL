96b26da318bc6394430a59d851c408e54a9003f2
KwonSunwon <kso00011147@gmail.com>
2023-05-23 15:08:05 +0900

    12주2: container 들의 찾기 속도 비교

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,47 +1,126 @@
 //-------------------------------------------------------
 // 2023 1학기 STL 5월 23일 (12주 2)
 //-------------------------------------------------------
-// unordered associative containers - 순서없는 연관 컨테이너
-// 1. 순서없다는 것을 관찰
-// 2. 메모리 구조를 화면에 출력한다
-// 3. String을 여기에 원소로 집어넣으려면?
+// 컨테이너 찾기 실력 검증. int값 1천만개, 찾을 int값 1만개
+// vector
+// set
+// unordered_set
 //-------------------------------------------------------
 // 코딩환경 - VS Release/x64, C++표준 - latest, SDL/아니오
 //-------------------------------------------------------
 
 #include <iostream>
+#include <array>
+#include <vector>
+#include <set>
 #include <unordered_set>
+#include <algorithm>
+#include <random>
+#include <chrono>
 #include "save.h"
 #include "String.h"
 
 using namespace std;
 
-void print(const auto& us)
-{
-	for (int i = 0; i < us.bucket_count(); ++i) {
-		cout << "[" << i << "]";
-		for (auto p = us.begin(i); p != us.end(i); ++p)
-			cout << " - " << *p;
-		cout << endl;
-	}
-	cout << "-----------------------------------------------------" << endl;
-	for (const auto& n : us)
-		cout << n << ' ';
-	cout << endl << endl;
-}
+const int NUM = 10'000'000;
+const int FNUM = 10'000;
 
-template <>
-struct hash<String> {
-	size_t operator()(const String& str) const {
-		return hash<string>()(str.getString());
-	}
-};
+array<int, NUM> numbers;
+array<int, FNUM> toFind;
+
+default_random_engine dre;
+uniform_int_distribution uid{ 1, 100'000'000 };
 
 int main()
 {
-	unordered_set<String, hash<String>> us{"C++", "STL", "Container", "Iterator", "Algorithm"};
+	for (int& num : numbers)
+		num = uid(dre);
+	for (int& num : toFind)
+		num = uid(dre);
 
-	print(us);
+	{	// vector 찾기 실력 검증
+		vector<int> v{numbers.begin(), numbers.end()};
+
+		cout << "벡터에서 찾는 중...";
+		int cnt{ 0 };
+
+		// 시간재기 시작
+		auto start = chrono::steady_clock::now();
+		for (int num : toFind)
+			if (find(v.begin(), v.end(), num) != v.end())
+				++cnt;
+		// 시간재기 끝
+		auto end = chrono::steady_clock::now();
+		auto elapsedTime = chrono::duration_cast<chrono::microseconds>(end - start).count();
+		cout << endl;
+		cout << "걸린 시간 - " << elapsedTime << "us" << endl;
+		cout << "찾은 개수 - " << cnt << endl;
+	}
+	cout << endl;
+
+	{	// set 찾기 실력 검증
+		cout << "셋 만드는 중..." << endl;
+		set<int> s{numbers.begin(), numbers.end()};
+
+		cout << "셋에서 찾는 중...";
+		int cnt{ 0 };
+
+		// 시간재기 시작
+		auto start = chrono::steady_clock::now();
+		for (int num : toFind)
+			if (s.contains(num))
+				cnt++;
+		// 시간재기 끝
+		auto end = chrono::steady_clock::now();
+		auto elapsedTime = chrono::duration_cast<chrono::microseconds>(end - start).count();
+		cout << endl;
+		cout << "걸린 시간 - " << elapsedTime << "us" << endl;
+		cout << "찾은 개수 - " << cnt << endl;
+	}
+	cout << endl;
+
+	{	// unordered_set 찾기 실력 검증
+		cout << "언오더드셋 만드는 중..." << endl;
+		unordered_set<int> us{numbers.begin(), numbers.end()};
+
+		cout << "언오더드셋에서 찾는 중...";
+		int cnt{ 0 };
+
+		// 시간재기 시작
+		auto start = chrono::steady_clock::now();
+		for (int num : toFind)
+			if (us.contains(num))
+				cnt++;
+		// 시간재기 끝
+		auto end = chrono::steady_clock::now();
+		auto elapsedTime = chrono::duration_cast<chrono::microseconds>(end - start).count();
+		cout << endl;
+		cout << "걸린 시간 - " << elapsedTime << "us" << endl;
+		cout << "찾은 개수 - " << cnt << endl;
+	}
+	cout << endl;
+	
+	{	// 정렬한 vector 찾기 실력 검증
+		vector<int> v{numbers.begin(), numbers.end()};
+		cout << "벡터 정렬하는 중..." << endl;
+		sort(v.begin(), v.end());
+
+		cout << "정렬한 벡터에서 찾는 중...";
+		int cnt{ 0 };
+
+		// 시간재기 시작
+		auto start = chrono::steady_clock::now();
+		for (int num : toFind)
+			if (binary_search(v.begin(), v.end(), num))
+				++cnt;
+		// 시간재기 끝
+		auto end = chrono::steady_clock::now();
+		auto elapsedTime = chrono::duration_cast<chrono::microseconds>(end - start).count();
+		cout << endl;
+		cout << "걸린 시간 - " << elapsedTime << "us" << endl;
+		cout << "찾은 개수 - " << cnt << endl;
+	}
+	cout << endl;
 
 	//save("main.cpp");
 }
